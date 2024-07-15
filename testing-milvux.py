18787247d62e668086a747bdec9df282992fbe03
vansh-pundir-rc <vansh.pundir@roundcircle.tech>
2024-07-15 12:40:03 +0530

    eheh

--- testing-milvux.py
+++ testing-milvux.py
@@ -1,10 +1,5 @@
-
-from pymilvus import Collection
-collection = Collection("medium_articles")      # Get an existing collection.
-collection.load()
-
-res = collection.query(
-  expr = "book_id in [2,4,6,8]",
-  output_fields = ["book_id", "book_intro"],
-  consistency_level="Strong"
-)
+# Load html
+# OpenAI
+#  LLMChain
+# context
+# return dictionary
