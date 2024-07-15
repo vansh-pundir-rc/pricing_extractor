2e3a57149de9b1620e8d7c1b34074923ad5743e2
vansh-pundir-rc <vansh.pundir@roundcircle.tech>
2024-07-15 12:43:44 +0530

    added code

--- testing-milvux.py
+++ testing-milvux.py
@@ -2,4 +2,23 @@
 # OpenAI
 #  LLMChain
 # context
-# return dictionary
+# return dictionar
+
+from langchain.document_loaders import UnstructuredHTMLLoader
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+
+load = UnstructuredHTMLLoader("test.html")
+data = load.load()
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Extract pricing information along with the catalogue which has comparison in a structure dictionary format"),
+        ("user", "{context}")
+    ]
+)
+chain = prompt | ChatOpenAI(model_name = "gpt-4o")
+
+chain.invoke({"context": data})
+
